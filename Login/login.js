42a1aeda4927a67c780c76315c106a84811a22a1
Dhruv Vohra <dhruv.v405@gmail.com>
2018-11-04 17:40:50 +0530

    Organised folders for Phase-2

--- Login/login.js
+++
@@ -1,8 +0,0 @@
-function showPassword() {
-    var x = document.getElementById("pwd");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-}
