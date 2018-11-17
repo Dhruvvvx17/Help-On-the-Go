f4c7cc4f3d6f975d233950a072feba5446ad3807
Dev Bhartra <devbhartra183@gmail.com>
2018-11-17 19:20:35 +0530

    signup worker once again has a functioning backend

--- Project Files/php/signup_worker.php
+++ Project Files/php/signup_worker.php
@@ -1,22 +1,29 @@
 <?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
     // obatining all fields from the form
-    $firstname = $_POST['firstName'];
-    $lastname = $_POST['lastName'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];       
-    $phone = $_POST['phone'];
-    $workArea = $_POST['workArea'];
-    $apartment = $_POST['apartment'];  
-    $workType = $_POST['workType'];
-    $charges = $_POST['charges'];
-    // $fromTime	 //doubts over here
-    $hours = $_POST['hours'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    // if(isset($_POST['SUBMIT'])){
+        $firstname = $_POST['firstName'];
+        $lastname = $_POST['lastName'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $workArea = $_POST['workArea'];
+        // $workArea = 'ITPL';
+        $apartment = $_POST['apartment'];
+        $workType = $_POST['workType'];
+        $charges = $_POST['charges'];
+        // $fromTime	 //doubts over here
+        $hours = $_POST['hours'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // echo "$firstname";
+    // }
+
 
 
     // To check the form details once php script loads
-    echo $firstname;
+    echo "$firstname";
     echo "<br>";
     echo $lastname;
     echo "<br>";
@@ -34,19 +41,15 @@
     echo "<br>";
     echo $charges;
     echo "<br>";
-    // echo $fromTime;     //doubts here as well
-    // echo "<br>";
-    // echo $toTime;
-    // echo "<br>";
     echo $hours;
     echo "<br>";
     echo $username;
     echo "<br>";
     echo $password;
     echo "<br>";
-    
+
     // Steps to run this php code:
-    // 1.Turn on MYSQL through xampp server.
+    // 1.Turn on MYSQL through xampp server.xus
     // 2.Create a database called 'testdb'
     // 3.Create a table called 'usersignup'
     // 4.Run the 'signup_user.html' file on browser to fill the user signup form.
@@ -65,10 +68,10 @@
     }
     else{
         echo "<br>Connected<br>";
-        $SELECT = "SELECT email From usersignup Where email = ? Limit 1";
-        $INSERT = "INSERT Into usersignup (firstname,lastname,,gender,email,phone,workArea,apartment,workType,charges,hours,username,password) values(?,?,?,?,?,?,?,?,?,?,?,?)";    
+        $SELECT = "SELECT email From signup_worker Where email = ? Limit 1";
+        $INSERT = "INSERT Into signup_worker (firstname,lastname,gender,phone,email,workArea,apartment,workType,charges,hours,username,password) values(?,?,?,?,?,?,?,?,?,?,?,?)";
     }
-    
+
     // Prepare statement
     $stmt = $conn->prepare($SELECT);
     $stmt->bind_param("s",$email);
@@ -80,9 +83,8 @@
 
     if($rnum==0){
         $stmt->close();
-
         $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("sssisss",$firstname,$lastname,$email,$phone,$addr,$username,$pw);
+        $stmt->bind_param("ssssisssiiss",$firstname,$lastname,$gender,$email,$phone,$workArea,$apartment,$workType,$charges,$hours,$username,$password);
         $stmt->execute();
         echo "New Record inserted successfully";
     }
