7a888792d52d86af7b284677bc0cd04b11a33455
Dev Bhartra <devbhartra183@gmail.com>
2018-11-18 14:08:54 +0530

    linked all the service slide to worker page

--- Project Files/trythis.php
+++ Project Files/trythis.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "wtproj";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
